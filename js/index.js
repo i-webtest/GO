af37b891315219d30c16ab974f98b2fdba2fbe0f
i-webtest <stasbass80@gmail.com>
2023-04-14 20:45:40 +0300

    Fixed bugs

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 import './module/jquery-3.6.4.min.js';
-import './module/mobileMenu.js';
 import './module/modalForm.js';
 import './module/modalController.js';
+import './module/mobileMenu.js';
--- js/module/mobileMenu.js
+++ js/module/mobileMenu.js
@@ -1,61 +1,38 @@
-import scrollController from './scrollController.js';
+$('.burger').on('click', function () {
+  $('.burger, .navigation').toggleClass('active');
+  $('body').toggleClass('lock');
+});
 
-const burgerBtn = $('.burger');
-const navigation = $('.navigation-mobile');
-const orderBtn = $('.order-mobile');
-const navLink = $('.navigation__link');
-
-// const toggleMenu = () => {
-//   navigation.toggle('navigation_active');
-// };
-
-const openMenu = () => {
-  navigation.slideDown();
-  burgerBtn.addClass('burger_active');
-  scrollController.disabledScroll();
-};
-
-const closeMenu = () => {
-  navigation.slideUp();
-  burgerBtn.removeClass('burger_active');
-  scrollController.enabledScroll();
-};
-
-burgerBtn.click(function () {
-  if (burgerBtn.hasClass('burger_active')) {
-    closeMenu();
-    // toggleMenu();
-  } else {
-    openMenu();
-    // toggleMenu();
+const deleteClass = () => {
+  if ($('.burger, .navigation').hasClass('active')) {
+    $('.burger, .navigation').removeClass('active');
+    $('body').removeClass('lock');
   }
-});
+};
 
-orderBtn.click(function () {
-  closeMenu();
-});
+$('.navigation__link').on('click', deleteClass);
 
-navLink.click(function () {
-  closeMenu();
-});
+$('.order').on('click', deleteClass);
 
-$(document).click(function (e) {
+$(document).on('click', function (e) {
   const target = e.target;
 
-  if (
-    !(
-      $(target).parents('.navigation__list').length ||
-      $(target).hasClass('navigation__list') ||
-      $(target).hasClass('burger')
-    )
-  ) {
-    closeMenu();
-    // toggleMenu();
+  if ($('.burger, .navigation').hasClass('active')) {
+    if (
+      !(
+        $(target).parents('.navigation__list').length ||
+        $(target).hasClass('navigation__list') ||
+        $(target).hasClass('burger')
+      )
+    ) {
+      $('.burger, .navigation').removeClass('active');
+      $('body').removeClass('lock');
+    }
   }
 });
 
 $(document).on('keyup', function (e) {
   if (e.key == 'Escape') {
-    closeMenu();
+    deleteClass();
   }
 });
